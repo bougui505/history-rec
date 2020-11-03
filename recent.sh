2b989f478a3c6912f592decaabb48cf2d4f56eed
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-11-04 00:26:22 +0100

    new file:   recent.sh

---
+++ recent.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+# -*- coding: UTF8 -*-
+
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr
+# https://research.pasteur.fr/en/member/guillaume-bouvier/
+# 2020-11-04 00:08:55 (UTC+0100)
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe failsi
+set -o noclobber  # prevent overwritting redirection
+
+N=20
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -n) N="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+
+OUT=$(recsel -R date,return_val,command $HOME/.history.rec | sed '/^[[:space:]]*$/d')
+echo $OUT | tail -n$N | awk '{if ($2>0){print "\033[31m"$0"\033[0m"} else{print $0}}'
